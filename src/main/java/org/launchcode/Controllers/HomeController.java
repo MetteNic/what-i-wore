da9d1b4e33a067dcbcacfbc31e3a870fb16f4db2
Mette Nichols <quiteunicorn@gmail.com>
2017-04-12 17:10:56 -0500

    added homeController

---
+++ src/main/java/org/launchcode/Controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.launchcode.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by mettenichols on 4/12/17.
+ */
+@Controller
+public class HomeController {
+
+        @RequestMapping(value = "")
+        public String index(Model model) {
+            model.addAttribute("title", "What I Wore");
+            return "index";
+        }
+
+    }
+
