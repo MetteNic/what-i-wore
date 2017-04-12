c5888b852ac684a2ef25c484b5b51922bd1fd92f
Mette Nichols <quiteunicorn@gmail.com>
2017-04-12 18:23:44 -0500

    added Outfit to Models

---
+++ src/main/java/org/launchcode/Models/Outfit.java
@@ -0,0 +1,53 @@
+package org.launchcode.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by mettenichols on 4/12/17.
+ */
+@Entity
+public class Outfit {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @NotNull
+    @Size(min =1, message = "please provide a description ")
+    private String description;
+
+    Outfit(){};
+
+    Outfit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
